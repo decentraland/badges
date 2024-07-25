370ba3cd86bc7f0b779a0681c84a8caddea66014
Alejo Thomas Ortega <alejothomasortega@gmail.com>
2024-07-25 12:11:24 -0300

    feat: add /status and /health/live endpoints (#16)

--- processor/src/controllers/routes.ts
+++ processor/src/controllers/routes.ts
@@ -1,14 +1,12 @@
 import { Router } from '@well-known-components/http-server'
 import { GlobalContext } from '../types'
 import { errorHandler } from '@dcl/platform-server-commons'
-import { getUserBadgesHandler } from './handlers/get-user-badges'
 import { getStatusHandler } from './handlers/get-service-status'
 
 export async function setupRouter(_: GlobalContext): Promise<Router<GlobalContext>> {
   const router = new Router<GlobalContext>()
   router.use(errorHandler)
 
-  router.get('/badges/:address', getUserBadgesHandler)
   router.get('/status', getStatusHandler)
 
   return router
