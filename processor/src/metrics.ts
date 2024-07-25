70afccca0fac1fcb377906191743fdeced17a862
Alejo Thomas Ortega <alejo.ortega@decentraland.org>
2024-07-25 16:35:53 -0300

    fix: metrics on processor

--- processor/src/metrics.ts
+++ processor/src/metrics.ts
@@ -1,7 +1,11 @@
+import { getDefaultHttpMetrics } from '@well-known-components/http-server'
 import { metricDeclarations as logMetricDeclarations } from '@well-known-components/logger'
+import { metricDeclarations as pgMetricDeclarations } from '@well-known-components/pg-component'
 import { validateMetricsDeclaration } from '@well-known-components/metrics'
 
 export const metricDeclarations = {
+  ...getDefaultHttpMetrics(),
+  ...pgMetricDeclarations,
   ...logMetricDeclarations
 }
 
