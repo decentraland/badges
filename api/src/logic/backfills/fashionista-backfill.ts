c67a98aa3c7cc841535e6629c3c75eac7361093f
Alejo Thomas Ortega <alejo.ortega@decentraland.org>
2024-09-06 17:50:40 -0300

    debug

--- api/src/logic/backfills/fashionista-backfill.ts
+++ api/src/logic/backfills/fashionista-backfill.ts
@@ -40,66 +40,69 @@ export function mergeFashionistaProgress(
     achieved_tiers: []
   }
 
-  console.log({
-    userAddress,
-    backfillData: JSON.stringify(backfillData),
-    userProgress: JSON.stringify(userProgress)
-  })
+  try {
+    const uniqueTransactions = new Set<string>([
+      ...userProgress.progress.transactions_wearables_purchase,
+      ...backfillData.progress.wearablesBought.map((wearable: any) => wearable.transactionHash)
+    ])
 
-  const uniqueTransactions = new Set<string>([
-    ...userProgress.progress.transactions_wearables_purchase,
-    ...backfillData.progress.wearablesBought.map((wearable: any) => wearable.transactionHash)
-  ])
+    const sortedBuys = backfillData.progress.wearablesBought.sort((a: any, b: any) => a.saleAt - b.saleAt)
 
-  const sortedBuys = backfillData.progress.wearablesBought.sort((a: any, b: any) => a.saleAt - b.saleAt)
+    userProgress.progress = {
+      steps: uniqueTransactions.size,
+      transactions_wearables_purchase: Array.from(uniqueTransactions)
+    }
 
-  userProgress.progress = {
-    steps: uniqueTransactions.size,
-    transactions_wearables_purchase: Array.from(uniqueTransactions)
-  }
-
-  // this badge has tiers
-  const achievedTiers = badge.tiers!.filter((tier) => {
-    return userProgress.progress.steps >= tier.criteria.steps
-  })
-
-  if (achievedTiers.length > 0) {
-    userProgress.achieved_tiers = achievedTiers.map((tier) => {
-      const transactionFound = sortedBuys[tier.criteria.steps - 1]
-      const userAlreadyHadThisTier = userProgress.achieved_tiers!.find(
-        (achievedTier) => achievedTier.tier_id === tier.tierId
-      )
-
-      if (transactionFound) {
-        return {
-          tier_id: tier.tierId,
-          completed_at:
-            userAlreadyHadThisTier && userAlreadyHadThisTier.completed_at < transactionFound.saleAt
-              ? userAlreadyHadThisTier.completed_at
-              : transactionFound.saleAt
-        }
-      } else {
-        return {
-          tier_id: tier.tierId,
-          completed_at: userAlreadyHadThisTier ? userAlreadyHadThisTier.completed_at : Date.now()
-        }
-      }
+    // this badge has tiers
+    const achievedTiers = badge.tiers!.filter((tier) => {
+      return userProgress.progress.steps >= tier.criteria.steps
     })
-  }
 
-  if (userProgress.achieved_tiers?.length === badge.tiers?.length) {
-    const lastTier = badge.tiers?.pop()
-    const alreadyAchievedDate = userProgress.completed_at
-    const foundRelatedSortedBuy = sortedBuys[lastTier?.criteria.steps - 1]
+    if (achievedTiers.length > 0) {
+      userProgress.achieved_tiers = achievedTiers.map((tier) => {
+        const transactionFound = sortedBuys[tier.criteria.steps - 1]
+        const userAlreadyHadThisTier = userProgress.achieved_tiers!.find(
+          (achievedTier) => achievedTier.tier_id === tier.tierId
+        )
 
-    if (foundRelatedSortedBuy && (!alreadyAchievedDate || foundRelatedSortedBuy.saleAt < alreadyAchievedDate)) {
-      userProgress.completed_at = foundRelatedSortedBuy.saleAt
+        if (transactionFound) {
+          return {
+            tier_id: tier.tierId,
+            completed_at:
+              userAlreadyHadThisTier && userAlreadyHadThisTier.completed_at < transactionFound.saleAt
+                ? userAlreadyHadThisTier.completed_at
+                : transactionFound.saleAt
+          }
+        } else {
+          return {
+            tier_id: tier.tierId,
+            completed_at: userAlreadyHadThisTier ? userAlreadyHadThisTier.completed_at : Date.now()
+          }
+        }
+      })
     }
 
-    if (!foundRelatedSortedBuy && !alreadyAchievedDate) {
-      userProgress.completed_at = Date.now()
-    }
-  }
+    if (userProgress.achieved_tiers?.length === badge.tiers?.length) {
+      const lastTier = badge.tiers?.pop()
+      const alreadyAchievedDate = userProgress.completed_at
+      const foundRelatedSortedBuy = sortedBuys[lastTier?.criteria.steps - 1]
 
-  return userProgress
+      if (foundRelatedSortedBuy && (!alreadyAchievedDate || foundRelatedSortedBuy.saleAt < alreadyAchievedDate)) {
+        userProgress.completed_at = foundRelatedSortedBuy.saleAt
+      }
+
+      if (!foundRelatedSortedBuy && !alreadyAchievedDate) {
+        userProgress.completed_at = Date.now()
+      }
+    }
+
+    return userProgress
+  } catch (error: any) {
+    console.log({
+      userAddress,
+      backfillData: JSON.stringify(backfillData),
+      userProgress: JSON.stringify(userProgress)
+    })
+    throw error
+  }
 }
