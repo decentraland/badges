913af393065cee4b3893458944dbd2274b46a3a4
Kevin Szuchet <kevin.szuchet@decentraland.org>
2024-09-13 12:41:57 +0100

    feat: Add the new observer and handler

--- processor/src/components.ts
+++ processor/src/components.ts
@@ -34,6 +34,7 @@ import { createProfileProObserver } from './logic/badges/profile-pro'
 import { createEmotionistaObserver } from './logic/badges/emotionista'
 import { createFashionistaObserver } from './logic/badges/fashionista'
 import { createMovesMasterObserver } from './logic/badges/moves-master'
+import { createSocialButterflyObserver } from './logic/badges/social-butterfly'
 
 // Initialize all the components of the app
 export async function initComponents(): Promise<AppComponents> {
@@ -107,6 +108,7 @@ export async function initComponents(): Promise<AppComponents> {
   eventDispatcher.registerObserver(createEmotionistaObserver({ db, logs, badgeStorage }))
   eventDispatcher.registerObserver(createFashionistaObserver({ db, logs, badgeStorage }))
   eventDispatcher.registerObserver(createMovesMasterObserver({ db, logs, badgeStorage }))
+  eventDispatcher.registerObserver(createSocialButterflyObserver({ db, logs, badgeStorage }))
 
   const eventParser = await createEventParser({ config, fetch })
 
---
+++ processor/src/logic/badges/social-butterfly.ts
@@ -0,0 +1,80 @@
+import { Badge, BadgeId, BadgeTier, UserBadge } from '@badges/common'
+import { AppComponents, BadgeProcessorResult, IObserver } from '../../types'
+import { EthAddress, Events, PassportOpenedEvent } from '@dcl/schemas'
+
+export function createSocialButterflyObserver({
+  db,
+  logs,
+  badgeStorage
+}: Pick<AppComponents, 'db' | 'logs' | 'badgeStorage'>): IObserver {
+  const logger = logs.getLogger('social-butterfly-badge')
+  const badgeId: BadgeId = BadgeId.SOCIAL_BUTTERFLY
+  const badge: Badge = badgeStorage.getBadge(badgeId)
+  const tieredBadges = badge.tiers!
+
+  async function handle(event: PassportOpenedEvent): Promise<BadgeProcessorResult | undefined> {
+    const userAddress = event.metadata.userAddress
+    const receiverAddress = event.metadata.passport.receiver
+
+    const userProgress: UserBadge = (await db.getUserProgressFor(badgeId, userAddress)) || initProgressFor(userAddress)
+
+    if (userProgress.completed_at) {
+      logger.info('User already has badge', { userAddress, badgeId })
+      return undefined
+    }
+
+    const visitedProfiles = new Set(userProgress.progress.profiles_visited)
+
+    if (visitedProfiles.has(receiverAddress)) {
+      return undefined
+    }
+
+    visitedProfiles.add(receiverAddress)
+    userProgress.progress.last_used_emote_timestamp = Array.from(visitedProfiles)
+    userProgress.progress.steps += 1
+
+    const newAchievedTier: BadgeTier | undefined = badge.tiers!.find(
+      (tier) =>
+        userProgress.progress.steps >= tier.criteria.steps &&
+        !userProgress.achieved_tiers!.find((achievedTier) => achievedTier?.tier_id === tier.tierId)
+    )
+
+    if (newAchievedTier) {
+      userProgress.achieved_tiers?.push({
+        tier_id: newAchievedTier.tierId,
+        completed_at: Date.now()
+      })
+    }
+
+    if (userProgress.achieved_tiers!.length === tieredBadges.length) {
+      userProgress.completed_at = Date.now()
+    }
+
+    await db.saveUserProgress(userProgress)
+
+    return newAchievedTier ? { badgeGranted: { ...badge, tiers: [newAchievedTier] }, userAddress } : undefined
+  }
+
+  function initProgressFor(userAddress: EthAddress): Omit<UserBadge, 'updated_at'> {
+    return {
+      user_address: userAddress,
+      badge_id: badgeId,
+      progress: {
+        steps: 0,
+        profiles_visited: []
+      },
+      achieved_tiers: []
+    }
+  }
+
+  return {
+    handle,
+    badge,
+    events: [
+      {
+        type: Events.Type.CLIENT,
+        subType: Events.SubType.Client.USED_EMOTE
+      }
+    ]
+  }
+}
