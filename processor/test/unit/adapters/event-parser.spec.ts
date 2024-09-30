44dd0e050ddbbd2c7e1613e7a7ee8dd57e3c1513
Alejo Thomas Ortega <alejo.ortega@decentraland.org>
2024-09-30 18:51:44 -0300

    fix: tests

--- processor/test/unit/adapters/event-parser.spec.ts
+++ processor/test/unit/adapters/event-parser.spec.ts
@@ -8,8 +8,8 @@ jest.mock('dcl-catalyst-client')
 
 describe('Event Parser', () => {
   it('should return undefined if it does not know the event to be parsed', async () => {
-    const { config, logs, fetch } = await getMockedComponents()
-    const parser = await createEventParser({ config, logs, fetch })
+    const { config, logs, badgeContext } = await getMockedComponents()
+    const parser = await createEventParser({ config, logs, badgeContext })
     const result = await parser.parse({ type: 'UnknownEvent' })
     expect(result).toBeUndefined()
   })
@@ -35,8 +35,8 @@ describe('Event Parser', () => {
     ]
   ])('when type is %s', (type: Events.Type, subTypes: SubType[]) => {
     it.each(subTypes)('and subtype %s should parse the event properly', async (subType) => {
-      const { config, logs, fetch } = await getMockedComponents()
-      const parser = await createEventParser({ config, logs, fetch })
+      const { config, logs, badgeContext } = await getMockedComponents()
+      const parser = await createEventParser({ config, logs, badgeContext })
       const event = { type, subType }
 
       const result = await parser.parse(event)
@@ -61,8 +61,8 @@ describe('Event Parser', () => {
     }
 
     it('and the entity type is not included in the subtypes should return undefined', async () => {
-      const { config, logs, fetch } = await getMockedComponents()
-      const parser = await createEventParser({ config, logs, fetch })
+      const { config, logs, badgeContext } = await getMockedComponents()
+      const parser = await createEventParser({ config, logs, badgeContext })
       const event = {
         entity: { entityId: 'some-id', entityType: 'non-existing-entity-type' }
       }
@@ -72,27 +72,19 @@ describe('Event Parser', () => {
     })
 
     it('and event does not have a content server url should use the load balancer url to fetch the entity', async () => {
-      const { config, logs, fetch } = await getMockedComponents()
-      const parser = await createEventParser({ config, logs, fetch })
+      const { config, logs, badgeContext } = await getMockedComponents()
+      const parser = await createEventParser({ config, logs, badgeContext })
       const event = {
         entity: { entityId: 'some-id', entityType: Events.SubType.CatalystDeployment.PROFILE }
       }
 
-      fetch.fetch = jest.fn().mockResolvedValue({
-        ok: true,
-        json: jest.fn().mockResolvedValue([mockEntity])
-      })
-
-      jest.spyOn(CatalystClient, 'createContentClient').mockReturnValue({
-        fetchEntityById: jest.fn().mockResolvedValue(mockEntity)
-      } as any)
+      badgeContext.getEntityById = jest.fn().mockResolvedValue(mockEntity)
 
       const result = await parser.parse(event)
       const loadBalancerUrl = await config.requireString('CATALYST_CONTENT_URL_LOADBALANCER')
 
-      expect(CatalystClient.createContentClient).toHaveBeenCalledWith({
-        fetcher: fetch,
-        url: loadBalancerUrl
+      expect(badgeContext.getEntityById).toHaveBeenCalledWith(event.entity.entityId, {
+        contentServerUrl: loadBalancerUrl
       })
 
       expect(result).toEqual({
@@ -107,17 +99,14 @@ describe('Event Parser', () => {
     it.each(Object.values(Events.SubType.CatalystDeployment))(
       'and the entity type is %s should parse the event properly',
       async (subType) => {
-        const { config, logs, fetch } = await getMockedComponents()
-        const parser = await createEventParser({ config, logs, fetch })
+        const { config, logs, badgeContext } = await getMockedComponents()
+        const parser = await createEventParser({ config, logs, badgeContext })
         const event = {
           entity: { entityId: 'some-id', entityType: subType },
           contentServerUrls: ['http://some-url']
         }
 
-        fetch.fetch = jest.fn().mockResolvedValue({
-          ok: true,
-          json: jest.fn().mockResolvedValue([mockEntity])
-        })
+        badgeContext.getEntityById = jest.fn().mockResolvedValue(mockEntity)
 
         const result = await parser.parse(event)
 
@@ -132,23 +121,22 @@ describe('Event Parser', () => {
     )
 
     it('and something goes wrong when fetching the entity should throw a ParsingEventError', async () => {
-      const { config, logs, fetch } = await getMockedComponents()
-      const parser = await createEventParser({ config, logs, fetch })
+      const { config, logs, badgeContext } = await getMockedComponents()
       const event = {
         entity: { entityId: 'some-id', entityType: Events.SubType.CatalystDeployment.PROFILE },
         contentServerUrls: ['http://some-url']
       }
 
-      jest.spyOn(CatalystClient, 'createContentClient').mockReturnValue({
-        fetchEntityById: jest.fn().mockRejectedValue(new Error('Error fetching entity from content server'))
-      } as any)
+      badgeContext.getEntityById = jest.fn().mockRejectedValue(new Error('Error fetching entity from content server'))
+      const parser = await createEventParser({ config, logs, badgeContext })
+
 
       await expect(parser.parse(event)).rejects.toThrow(ParsingEventError)
     })
   })
 
   // Helpers
-  async function getMockedComponents(): Promise<Pick<AppComponents, 'config' | 'fetch' | 'logs'>> {
+  async function getMockedComponents(): Promise<Pick<AppComponents, 'config' | 'logs' | 'badgeContext'>> {
     return {
       config: {
         requireString: jest.fn(),
@@ -156,10 +144,12 @@ describe('Event Parser', () => {
         getNumber: jest.fn(),
         requireNumber: jest.fn()
       },
-      fetch: {
-        fetch: jest.fn()
-      },
-      logs: await createLogComponent({ config: { requireString: jest.fn(), getString: jest.fn() } as any })
+      logs: await createLogComponent({ config: { requireString: jest.fn(), getString: jest.fn() } as any }),
+      badgeContext: {
+        getEntityById: jest.fn().mockResolvedValue({} as any),
+        getEntitiesByPointers: jest.fn().mockResolvedValue([] as any),
+        getWearablesWithRarity: jest.fn().mockResolvedValue([] as any)
+      }
     }
   }
 })
