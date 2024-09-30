065135ff5cf2e710d5160c7b1c11864b1e29fe32
Alejo Thomas Ortega <alejo.ortega@decentraland.org>
2024-09-30 11:38:52 -0300

    refactor: retry mechanism for Catalyst fetches

--- processor/src/adapters/badge-context.ts
+++ processor/src/adapters/badge-context.ts
@@ -2,7 +2,7 @@ import { Entity } from '@dcl/schemas'
 import { createContentClient } from 'dcl-catalyst-client'
 import { AppComponents, IBadgeContext } from '../types'
 import { getTokenIdAndAssetUrn, isExtendedUrn, parseUrn } from '@dcl/urn-resolver'
-import { sleep } from '../utils/timer'
+import retry from '../utils/retryer'
 
 export async function createBadgeContext({
   fetch,
@@ -37,21 +37,11 @@ export async function createBadgeContext({
   ): Promise<Entity> {
     const retries = options.retries ?? 3
     const waitTime = options.waitTime ?? 300
-    const contentClientToUser = options.contentServerUrl
+    const contentClientToUse = options.contentServerUrl
       ? createContentClient({ fetcher: fetch, url: options.contentServerUrl })
       : contentClient
 
-    for (let attempt = 1; attempt <= retries; attempt++) {
-      try {
-        return await contentClientToUser.fetchEntityById(entityId)
-      } catch (error: any) {
-        if (attempt === retries) {
-          throw new Error(`Failed to fetch entity after ${retries} attempts: ${error.message}`)
-        }
-        await sleep(waitTime)
-      }
-    }
-    throw new Error('Unexpected error: retry loop ended without throwing')
+    return retry(() => contentClientToUse.fetchEntityById(entityId), retries, waitTime)
   }
 
   async function getEntitiesByPointers(
@@ -64,18 +54,7 @@ export async function createBadgeContext({
       ? createContentClient({ fetcher: fetch, url: options.contentServerUrl })
       : contentClient
 
-    for (let attempt = 1; attempt <= retries; attempt++) {
-      try {
-        const fetchedEntities: Entity[] = await contentClientToUse.fetchEntitiesByPointers(pointers)
-        return fetchedEntities
-      } catch (error: any) {
-        if (attempt === retries) {
-          throw new Error(`Failed to fetch entity after ${retries} attempts: ${error.message}`)
-        }
-        await sleep(waitTime)
-      }
-    }
-    throw new Error('Unexpected error: retry loop ended without throwing')
+    return retry(() => contentClientToUse.fetchEntitiesByPointers(pointers), retries, waitTime)
   }
 
   return { getWearablesWithRarity, getEntityById, getEntitiesByPointers }
