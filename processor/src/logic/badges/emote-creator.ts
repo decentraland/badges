f7c490b8b74e8a2fc384ec2883537c0c69fb46eb
Alejo Thomas Ortega <alejo.ortega@decentraland.org>
2024-10-02 15:47:13 -0300

    fix: emote-creator badge handler to also save date of publishment

--- processor/src/logic/badges/emote-creator.ts
+++ processor/src/logic/badges/emote-creator.ts
@@ -37,8 +37,11 @@ export function createEmoteCreatorObserver({
     }
 
     const { itemId } = event.metadata
+    const emoteWasAlreadyPublished = userProgress.progress.published_emotes.find(
+      (publishedEmote: any) => publishedEmote.itemId === itemId
+    )
 
-    if (userProgress.progress.published_emotes.includes(itemId)) {
+    if (emoteWasAlreadyPublished) {
       logger.info('User already published this emote', {
         userAddress: userAddress,
         itemId
@@ -47,8 +50,10 @@ export function createEmoteCreatorObserver({
       return undefined
     }
 
-    userProgress.progress.published_emotes.push(itemId)
-    const uniqueEmotesPublished = new Set<string>(userProgress.progress.published_emotes)
+    userProgress.progress.published_emotes.push({ itemId, publishedAt: event.timestamp })
+    const uniqueEmotesPublished = new Set<{ itemId: string; publishedAt: number }>(
+      userProgress.progress.published_emotes
+    )
     userProgress.progress.steps = uniqueEmotesPublished.size
     userProgress.progress.published_emotes = Array.from(uniqueEmotesPublished)
 
@@ -81,7 +86,7 @@ export function createEmoteCreatorObserver({
       badge_id: badgeId,
       progress: {
         steps: 0,
-        published_emotes: []
+        published_emotes: [] as { itemId: string; publishedAt: number }[]
       },
       achieved_tiers: []
     }
