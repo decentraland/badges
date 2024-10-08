2453999c273901d3f688f76bd95ef979332c820b
Kevin Szuchet <kevin.szuchet@decentraland.org>
2024-10-08 11:53:32 +0100

    feat: Add Get Badge Details tests

---
+++ api/test/integration/get-badge-details.spec.ts
@@ -0,0 +1,43 @@
+import { BadgeId } from '@badges/common'
+import { test } from '../components'
+
+test('GET /badges/:id', function ({ components }) {
+  const endpointPath = (id: string) => `/badges/${id}`
+
+  it('should return 404 when it is not possible to parse the badge id', async function () {
+    const response = await components.localFetch.fetch(endpointPath('unknown'), {
+      method: 'GET',
+      redirect: 'manual',
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+
+    const body = await response.json()
+
+    expect(response.status).toBe(404)
+    expect(body).toMatchObject({
+      error: 'Badge not found'
+    })
+  })
+
+  it('should return the badge definition when the id is valid', async function () {
+    const badgeId = BadgeId.DECENTRALAND_CITIZEN
+    const response = await components.localFetch.fetch(endpointPath(badgeId), {
+      method: 'GET',
+      redirect: 'manual',
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+
+    const body = await response.json()
+
+    expect(response.status).toBe(200)
+    expect(body).toMatchObject({
+      data: {
+        badge: components.badgeService.getBadge(badgeId)
+      }
+    })
+  })
+})
