78666649599212169bf46b7233018a7a3a5027b9
Braian Mellor <braianj@gmail.com>
2024-11-07 00:58:36 -0300

    feat: add tiers enums

---
+++ common/src/types/tiers.ts
@@ -0,0 +1,100 @@
+enum TierTraveler {
+  STARTER = 'traveler-starter',
+  BRONZE = 'traveler-bronze',
+  SILVER = 'traveler-silver',
+  GOLD = 'traveler-gold',
+  PLATINUM = 'traveler-platinum',
+  DIAMOND = 'traveler-diamond'
+}
+
+enum TierEmotionista {
+  STARTER = 'emotionista-starter',
+  BRONZE = 'emotionista-bronze',
+  SILVER = 'emotionista-silver',
+  GOLD = 'emotionista-gold',
+  PLATINUM = 'emotionista-platinum',
+  DIAMOND = 'emotionista-diamond'
+}
+
+enum TierFashionista {
+  STARTER = 'fashionista-starter',
+  BRONZE = 'fashionista-bronze',
+  SILVER = 'fashionista-silver',
+  GOLD = 'fashionista-gold',
+  PLATINUM = 'fashionista-platinum',
+  DIAMOND = 'fashionista-diamond'
+}
+
+enum TierEventEnthusiast {
+  STARTER = 'event-enthusiast-starter',
+  BRONZE = 'event-enthusiast-bronze',
+  SILVER = 'event-enthusiast-silver',
+  GOLD = 'event-enthusiast-gold',
+  PLATINUM = 'event-enthusiast-platinum',
+  DIAMOND = 'event-enthusiast-diamond'
+}
+
+enum TierMovesMaster {
+  STARTER = 'moves-master-starter',
+  BRONZE = 'moves-master-bronze',
+  SILVER = 'moves-master-silver',
+  GOLD = 'moves-master-gold',
+  PLATINUM = 'moves-master-platinum',
+  DIAMOND = 'moves-master-diamond'
+}
+
+enum TierSocialButterfly {
+  STARTER = 'social-butterfly-starter',
+  BRONZE = 'social-butterfly-bronze',
+  SILVER = 'social-butterfly-silver',
+  GOLD = 'social-butterfly-gold',
+  PLATINUM = 'social-butterfly-platinum',
+  DIAMOND = 'social-butterfly-diamond'
+}
+
+enum TierWalkaboutWanderer {
+  STARTER = 'walkabout-wanderer-starter',
+  BRONZE = 'walkabout-wanderer-bronze',
+  SILVER = 'walkabout-wanderer-silver',
+  GOLD = 'walkabout-wanderer-gold',
+  PLATINUM = 'walkabout-wanderer-platinum',
+  DIAMOND = 'walkabout-wanderer-diamond'
+}
+
+enum TierEmoteCreator {
+  STARTER = 'emote-creator-starter',
+  BRONZE = 'emote-creator-bronze',
+  SILVER = 'emote-creator-silver',
+  GOLD = 'emote-creator-gold',
+  PLATINUM = 'emote-creator-platinum',
+  DIAMOND = 'emote-creator-diamond'
+}
+
+enum TierWearableDesigner {
+  STARTER = 'wearable-designer-starter',
+  BRONZE = 'wearable-designer-bronze',
+  SILVER = 'wearable-designer-silver',
+  GOLD = 'wearable-designer-gold',
+  PLATINUM = 'wearable-designer-platinum',
+  DIAMOND = 'wearable-designer-diamond'
+}
+
+enum TierMusicFestival {
+  FIRST_DAY = 'music-festival-first-day',
+  SECOND_DAY = 'music-festival-second-day',
+  THIRD_DAY = 'music-festival-third-day',
+  FINAL_DAY = 'music-festival-final-day'
+}
+
+export {
+  TierTraveler,
+  TierEmotionista,
+  TierFashionista,
+  TierEventEnthusiast,
+  TierMovesMaster,
+  TierSocialButterfly,
+  TierWalkaboutWanderer,
+  TierEmoteCreator,
+  TierWearableDesigner,
+  TierMusicFestival
+}
