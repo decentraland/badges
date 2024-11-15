14ffb740d1318d6b0afe97d957f29ab62fc157c3
Kevin Szuchet <kevin.szuchet@decentraland.org>
2024-11-15 14:34:39 +0000

    test: Add tests for the new context logic

--- processor/src/adapters/badge-context.ts
+++ processor/src/adapters/badge-context.ts
@@ -22,11 +22,6 @@ export async function createBadgeContext({
   const loadBalancer = await config.requireString('CATALYST_CONTENT_URL_LOADBALANCER')
   const contractNetwork = (await config.getString('ENV')) === 'prod' ? L1_MAINNET : L1_TESTNET
 
-  const contentClient = createContentClient({
-    fetcher: fetch,
-    url: loadBalancer
-  })
-
   async function getWearablesWithRarity(wearables: string[]): Promise<Entity[]> {
     const wearablesUrns: string[] = []
     for (const wearable of wearables) {
@@ -44,7 +39,12 @@ export async function createBadgeContext({
   }
 
   function getContentClientOrDefault(contentServerUrl?: string): ContentClient {
-    return contentServerUrl ? createContentClient({ fetcher: fetch, url: contentServerUrl }) : contentClient
+    return contentServerUrl
+      ? createContentClient({ fetcher: fetch, url: contentServerUrl })
+      : createContentClient({
+          fetcher: fetch,
+          url: loadBalancer
+        })
   }
 
   function rotateContentServerClient<T>(
@@ -52,14 +52,11 @@ export async function createBadgeContext({
     contentServerUrl?: string
   ) {
     const catalystServers = shuffleArray(getCatalystServersFromCache(contractNetwork)).map((server) => server.address)
+    let contentClientToUse: ContentClient = getContentClientOrDefault(contentServerUrl)
 
     return (attempt: number): Promise<T> => {
-      let contentClientToUse: ContentClient = getContentClientOrDefault(contentServerUrl)
-
       if (attempt > 1 && catalystServers.length > 0) {
-        const randomIndex = Math.floor(Math.random() * (catalystServers.length || 0))
-        const [catalystServerUrl] = catalystServers.splice(randomIndex, 1) || []
-
+        const [catalystServerUrl] = catalystServers.splice(attempt % catalystServers.length, 1) || []
         contentClientToUse = getContentClientOrDefault(catalystServerUrl)
       }
 
