81db3f876d8a7258611ace3aa9ba97368cbfc04e
Alejo Thomas Ortega <alejothomasortega@gmail.com>
2025-04-04 16:29:51 -0300

    feat: improve performance w/long-polling + metrics (#152)

--- processor/test/unit/logic/message-consumer.spec.ts
+++ processor/test/unit/logic/message-consumer.spec.ts
@@ -48,7 +48,7 @@ describe('MessageConsumer', () => {
     publisher = await createSnsComponent({ config })
     queue = {
       ...(await createSqsAdapter('endpoint')),
-      receiveSingleMessage: jest.fn(),
+      receiveMessages: jest.fn(),
       deleteMessage: jest.fn()
     }
     messageProcessor = await createMessageProcessorComponent({ logs, config, metrics, eventDispatcher, publisher })
@@ -60,7 +60,7 @@ describe('MessageConsumer', () => {
     messageHandle = 'receipt-handle-123'
     event = getMockEvent(Events.Type.BLOCKCHAIN, Events.SubType.Blockchain.ITEM_SOLD, testUserAddress)
     message = {
-      Body: JSON.stringify({ Message: JSON.stringify(event) }),
+      Body: JSON.stringify(event),
       ReceiptHandle: messageHandle
     }
   })
@@ -70,58 +70,56 @@ describe('MessageConsumer', () => {
   })
 
   it('should handle no messages found in the queue', async () => {
-    jest.spyOn(queue, 'receiveSingleMessage').mockResolvedValueOnce([])
+    jest.spyOn(queue, 'receiveMessages').mockResolvedValueOnce([])
 
     await consumeMessages()
-
-    expect(sleep).toHaveBeenCalled()
   })
 
   it('should process a valid message and remove it from the queue', async () => {
-    jest.spyOn(queue, 'receiveSingleMessage').mockResolvedValueOnce([message])
+    jest.spyOn(queue, 'receiveMessages').mockResolvedValueOnce([message])
     jest.spyOn(eventParser, 'parse').mockResolvedValue(event)
     jest.spyOn(messageProcessor, 'process').mockResolvedValue(undefined)
 
     await consumeMessages()
 
-    expect(queue.receiveSingleMessage).toHaveBeenCalled()
+    expect(queue.receiveMessages).toHaveBeenCalled()
     expect(eventParser.parse).toHaveBeenCalledWith(event)
     expect(messageProcessor.process).toHaveBeenCalledWith(event)
     expect(queue.deleteMessage).toHaveBeenCalledWith(messageHandle)
   })
 
   it('should remove the message from the queue without processing it when the message could not be parsed', async () => {
-    jest.spyOn(queue, 'receiveSingleMessage').mockResolvedValueOnce([message])
+    jest.spyOn(queue, 'receiveMessages').mockResolvedValueOnce([message])
     jest.spyOn(eventParser, 'parse').mockResolvedValue(undefined)
 
     await consumeMessages()
 
-    expect(queue.receiveSingleMessage).toHaveBeenCalled()
+    expect(queue.receiveMessages).toHaveBeenCalled()
     expect(eventParser.parse).toHaveBeenCalledWith(event)
     expect(messageProcessor.process).not.toHaveBeenCalled()
     expect(queue.deleteMessage).toHaveBeenCalledWith(messageHandle)
   })
 
   it('should remove a message from the queue without processing it when the parse fails', async () => {
-    jest.spyOn(queue, 'receiveSingleMessage').mockResolvedValueOnce([message])
+    jest.spyOn(queue, 'receiveMessages').mockResolvedValueOnce([message])
     jest.spyOn(eventParser, 'parse').mockRejectedValue(new Error('Failed to parse event'))
 
     await consumeMessages()
 
-    expect(queue.receiveSingleMessage).toHaveBeenCalled()
+    expect(queue.receiveMessages).toHaveBeenCalled()
     expect(eventParser.parse).toHaveBeenCalledWith(event)
     expect(messageProcessor.process).not.toHaveBeenCalled()
     expect(queue.deleteMessage).toHaveBeenCalledWith(messageHandle)
   })
 
   it('should process a valid message and remove it from the queue even when the message processor fails', async () => {
-    jest.spyOn(queue, 'receiveSingleMessage').mockResolvedValueOnce([message])
+    jest.spyOn(queue, 'receiveMessages').mockResolvedValueOnce([message])
     jest.spyOn(eventParser, 'parse').mockResolvedValue(event)
     jest.spyOn(messageProcessor, 'process').mockRejectedValue(new Error('Failed to process event'))
 
     await consumeMessages()
 
-    expect(queue.receiveSingleMessage).toHaveBeenCalled()
+    expect(queue.receiveMessages).toHaveBeenCalled()
     expect(eventParser.parse).toHaveBeenCalledWith(event)
     expect(messageProcessor.process).toHaveBeenCalledWith(event)
     expect(queue.deleteMessage).toHaveBeenCalledWith(messageHandle)
